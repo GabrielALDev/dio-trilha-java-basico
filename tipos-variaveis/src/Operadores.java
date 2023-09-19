63eb4774ff7ec8e478b110284981feb3b0492560
GabrielAL <gabrielcosta.alves@hotmail.com>
2023-09-19 20:28:20 -0300

    segundo-commit

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,18 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "Linguagem" + "Java";
+        System.out.println(nomeCompleto);
+
+
+        
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+        numero = numero * -1;
+
+        System.out.println(numero);
+    }
+}
---
+++ tipos-variaveis/src/Operadores01.java
@@ -0,0 +1,11 @@
+public class Operadores01 {
+    public static void main(String[] args) {
+    int numero = 5;
+    //x reperticao
+    //numero = numero + 2;
+
+    System.out.println( ++ numero);
+
+    System.out.println(numero);
+    }
+}
